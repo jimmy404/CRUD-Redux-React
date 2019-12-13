0c362a0e69b6d5fdfc2f2cd40f8bfc762042e355
Jimmy <bruno_pier@hotmail.com>
2019-12-13 00:45:18 -0300

    Adding new files & work on it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Productos from './components/Productos';
+import EditarProducto from './components/EditarProducto';
+import AgregarProducto from './components/AgregarProducto';
+import Producto from './components/Producto';
 
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Switch>
+          <Route exact path="/nuevo-producto" component={AgregarProducto} />
+          <Route exact path="/productos" component={Productos} />
+          <Route exact path="/productos/:id" component={Producto} />
+          <Route exact path="/productos/editar/:id" component={EditarProducto} />
+        </Switch>
+      </Router>
     </div>
   );
 }
