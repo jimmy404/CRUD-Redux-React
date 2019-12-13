2ee4dee5b04d5984bf1cdbd1a4f2676090a841ae
Jimmy <bruno_pier@hotmail.com>
2019-12-13 00:55:04 -0300

    Adding Header

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import Productos from './components/Productos';
 import EditarProducto from './components/EditarProducto';
 import AgregarProducto from './components/AgregarProducto';
 import Producto from './components/Producto';
+import Header from './components/Header';
 
 
 function App() {
   return (
-    <div className="App">
       <Router>
+          <Header />
+          <main className="container mt-5">
         <Switch>
           <Route exact path="/nuevo-producto" component={AgregarProducto} />
           <Route exact path="/productos" component={Productos} />
           <Route exact path="/productos/:id" component={Producto} />
           <Route exact path="/productos/editar/:id" component={EditarProducto} />
         </Switch>
+        </main>
+        <p className="mt-4 p2 text-center">Todos los derechos reservados</p>
       </Router>
-    </div>
   );
 }
 
