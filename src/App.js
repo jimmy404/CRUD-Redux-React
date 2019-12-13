1fec2c51d02554934bff1c0f19fbcd22d32ff57d
Jimmy <bruno_pier@hotmail.com>
2019-12-13 18:19:32 -0300

    Working on Route

--- src/App.js
+++ src/App.js
@@ -31,7 +31,12 @@ function App() {
           <Header />
             <main className="container mt-5">
         <Switch>
-          <Route exact path="/productos" component={Productos} />
+          <Route exact path="/productos" render={() => (
+            <Productos
+                productos={productos}
+              />
+          ) }
+          />
           <Route exact path="/nuevo-producto" component={AgregarProducto} />
           <Route exact path="/productos/:id" component={Producto} />
           <Route exact path="/productos/editar/:id" component={EditarProducto} />
---
+++ src/components/ProductoLista.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ProductoLista() {
+    return(
+            <div></div>
+    )
+}
+
+export default ProductoLista;
