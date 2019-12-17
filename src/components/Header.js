967631fcb83d38b85336c4094c8de15ec266dbc9
Jimmy <bruno_pier@hotmail.com>
2019-12-16 23:15:32 -0300

    Working in Link - Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link, NavLink} from 'react-router-dom';
 
+
 const Header = () => (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
